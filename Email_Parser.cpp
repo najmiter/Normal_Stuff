b56dd627a22c75d767860f3737a5cbf3b25e4a1b
Najam <85332859+najmiter@users.noreply.github.com>
2022-09-19 16:49:02 +0500

    Update Email_Parser.cpp

--- Email_Parser.cpp
+++ Email_Parser.cpp
@@ -1,35 +1,18 @@
 #include <iostream>
-
 int main(){
     std::string email = "realnajmiter@gmail.com";
     std::string Name, Company;
 
-    int DaThing, DaOtherThing;
+    int DaThing, DaOtherThing, i = 0;
 
-    for (size_t i = 0; i < email.length(); i++){
-        if (email[i] == '@') {
-            DaThing = i;
-            break;
-        }
-    }
-    for (size_t i = DaThing; i < email.length(); i++){
-        if (email[i] == '.') {
-            DaOtherThing = i-DaThing-1;
-            break;
-        }
-    }
+    DaThing = email.find('@');
+    DaOtherThing = email.find('.') - DaThing - 1;
 
     Name = email.substr(0, DaThing++);
     Company = email.substr(DaThing, DaOtherThing);
 
-    Name = char(Name[0] > 96 ? Name[0] - 32 : Name[0]) + Name.substr(1, Name.length()) ;
-
-    Company = char(Company[0] > 96 ? Company[0] - 32 : Company[0]) + Company.substr(1, Company.length()) ;
+    Name = char(Name[0] > 96 ? Name[0] - 32 : Name[0]) + Name.substr(1, Name.length());
+    Company = char(Company[0] > 96 ? Company[0] - 32 : Company[0]) + Company.substr(1, Company.length());
 
     std::cout<< "Hi " << Name << ", Welcome to " << Company << std::endl;
-
-    
-
-
-
 }
