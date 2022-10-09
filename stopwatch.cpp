b3c68da2d047ee65ca88a9e12843c0159a39de90
Najam <85332859+najmiter@users.noreply.github.com>
2022-10-09 23:12:14 +0500

    Update stopwatch.cpp

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -4,7 +4,10 @@
 #include <unistd.h>
 // For Windows:
     // #include<windows.h>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
 int main(){
     short minutes, seconds; int time;
 
@@ -18,15 +21,15 @@ int main(){
         cin>> minutes;
     } while (minutes>59);
 
-    time = (minutes!=0)? minutes*60 : 0;
+    time = minutes != 0 ? minutes*60 : 0;
     time += seconds;
 
-    for(int i = time; i>0 ; i--){
+    for(int i = time; i> 0 ; i--){
         system("clear");
         // system("cls"); // for Windows
         cout<< "The time remaining is 0:" << minutes << ":" << seconds << endl;
-        minutes = (i%60==0)? --minutes : minutes;
-        seconds = (seconds==0)? 59 : --seconds;
+        minutes = i % 60 == 0 ? --minutes : minutes;
+        seconds = seconds == 0 ? 59 : --seconds;
         // system("sleep 1");
         sleep(1); // the same effect.
         // system("TIMEOUT 1"); // kinda
@@ -34,5 +37,4 @@ int main(){
     system("clear");
     cout<< "*******************\nTimer ended!\a\nLasted for " << time << " seconds" << "\n*******************\n\n\n\n\n\n" << endl;
     
-    return 0;
 }
