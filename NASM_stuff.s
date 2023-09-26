9fa583cf24f4cacd990b7a4b366ceb412c70c718
Najam <85332859+najmiter@users.noreply.github.com>
2023-09-26 15:49:13 +0500

    Update NASM_stuff.s

--- NASM_stuff.s
+++ NASM_stuff.s
@@ -70,9 +70,6 @@ std__to_string:
     ;
     ; ----------------------------------------------------------------------
 
-section .bss
-    number resb 20 
-
 section .text
     push rsi              ; Keep the output string pointer on the stack for later
     push rax              ; Keep the value of RAX on the stack because the next loop will change its value
