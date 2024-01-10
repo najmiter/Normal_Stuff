b9b659a47d5ca64af70e382f82c6cf83f0617e47
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-10 12:35:27 +0500

    Update heavy_uog_lms_survey.js

--- heavy_uog_lms_survey.js
+++ heavy_uog_lms_survey.js
@@ -1,21 +1,27 @@
 // coded by the people for the people
 
 const thing = document.getElementById('resourceobject').contentWindow;
-let counter = 0;
-const total_subjects = 6; // Change to your number of subjects
-const delay = 2000; // You can change the <2000 ms> to something else depending upon your internet speed
-for (; counter < total_subjects * 2; counter++) {
+if (thing) {
+  let counter = 0;
+  const total_subjects = 6; // Change to your number of subjects
+  const delay = 2000; // You can change the <2000 ms> to something else depending upon your internet speed
+  for (; counter < total_subjects * 2; counter++) {
+    setTimeout(() => {
+      thing.document.querySelectorAll('input')
+      .forEach(each => {
+        if (each.value === '1' || each.value === '225') // could've just selected these with the css selectors but anyway 😁
+          each.checked = true;
+      })
+      
+      thing.document.querySelector(`input[type='submit']`).click();
+    }, counter * delay)
+  }
+  
   setTimeout(() => {
-    thing.document.querySelectorAll('input')
-    .forEach(each => {
-      if (each.value === '1' || each.value === '225') // could've just selected these with the css selectors but anyway 😁
-        each.checked = true;
-    })
-    
-    thing.document.querySelector(`input[type='submit']`).click();
-  }, counter * delay)
+    document.getElementById('actionmenuaction-6').click()
+  }, counter * delay);
+} else {
+  console.log(`Please make sure that you're:
+    1. Logged into your LMS.
+    2. Responsive Mode is turned off.`);
 }
-
-setTimeout(() => {
-  document.getElementById('actionmenuaction-6').click()
-}, counter * delay);
