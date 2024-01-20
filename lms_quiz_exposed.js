58941247dbe2f3af25e4fc9dd6d6d83bce6454d5
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-20 14:22:38 +0500

    Create lms_quiz_exposed.js

---
+++ lms_quiz_exposed.js
@@ -0,0 +1,11 @@
+const questions = document.querySelectorAll(".qtext");
+const answers = document.querySelectorAll(".answer");
+const t_question = questions.length;
+
+let all_ = "";
+
+for (let i = 0; i < t_question; i++) {
+    all_ += `${questions[i].innerText}\n${answers[i].innerText}\n\n`;
+}
+
+console.log(all_);
