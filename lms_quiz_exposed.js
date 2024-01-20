0572718196bdf4507cabd48e7e0ae30f9dde881f
Najam <85332859+najmiter@users.noreply.github.com>
2024-01-20 16:45:51 +0500

    Update lms_quiz_exposed.js

--- lms_quiz_exposed.js
+++ lms_quiz_exposed.js
@@ -5,7 +5,7 @@ const t_question = questions.length;
 let all_ = "";
 
 for (let i = 0; i < t_question; i++) {
-    all_ += `${questions[i]?.innerText}\n${answers[i]?.innerText}\n\n`;
+    all_ += `${questions[i]?.innerText ?? ""}\n${answers[i]?.innerText ?? ""}\n\n`;
 }
 
 console.log(all_);
